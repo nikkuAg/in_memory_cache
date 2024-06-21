8493e5f00a67310f98d25c9f450f689f4d1d47f3
Divyansh Agarwal <a.divyansh.25@gmail.com>
2024-06-22 02:48:29 +0530

    Add time to live value to cache data

--- cache/base_cache.py
+++ cache/base_cache.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from cache.eviction_policies import EvictionPolicyEnum
+import time
 
 
 class InMemoryCache:
@@ -8,8 +9,13 @@ class InMemoryCache:
         self.cache = OrderedDict()
         self.eviction_policy = eviction_policy()
 
-    def set(self, key, value):
-        """Set a value in the cache."""
+    def set(self, key, value, ttl=None):
+        """Set a value in the cache with an optional TTL (Time To Live)."""
+        if ttl:
+            expiry_time = time.time() + ttl
+            value = (value, expiry_time)
+
+        self.remove_expired_keys()
 
         if len(self.cache) >= self.memory:
             if key in self.cache:
@@ -23,11 +29,18 @@ class InMemoryCache:
         self.cache[key] = value
 
     def get(self, key):
-        """Get a value from the cache. Return None if key is not present."""
+        """Get a value from the cache. Return None if the key is not found or expired."""
         value = self.cache.get(key, None)
 
+        # Return None if the cache has expired
+        if value and isinstance(value, tuple):
+            value, expiry_time = value
+            if time.time() >= expiry_time:
+                del self.cache(key)
+            return None
+
+        # Update key order for recently used keys
         if value is not None and isinstance(self.eviction_policy, (EvictionPolicyEnum.LRU, EvictionPolicyEnum.MRU)):
-            # Update key order for recently used keys
             self.cache.move_to_end(key)
 
         return value
@@ -40,3 +53,11 @@ class InMemoryCache:
     def clear(self):
         """Clear all values from the cache."""
         self.cache.clear()
+
+    def remove_expired_keys(self):
+        """Remove all expired values from the cache."""
+        current_time = time.time()
+        expired_keys = [key for key, value in self.cache.items(
+        ) if isinstance(value, tuple) and value[1] < current_time]
+        for key in expired_keys:
+            del self.cache[key]
